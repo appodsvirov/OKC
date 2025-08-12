ad80e50386defe9a83a7c80ff1fd0ae9490e851a
mr_big_cake <mr_bi@DESKTOP-HQ8EIJJ>
2025-08-12 23:25:16 +0300

    + json

--- OKC.Console/Program.cs
+++ OKC.Console/Program.cs
@@ -1,8 +1,8 @@
 ﻿using OKC.Infrastructure.Services;
 
 var parser = new HtmlQuestionParser();
-var repository = new JsonQuestionRepository(Path.GetFullPath("..\\..\\..\\..\\outputs\\IV-до-1000В.json"));
+var repository = new JsonQuestionRepository(Path.GetFullPath("..\\..\\..\\..\\outputs\\IV-до-и-выше-1000В.json"));
 var processor = new QuestionProcessor(parser, repository);
 
-var htmlFiles = Directory.GetFiles(Path.GetFullPath("..\\..\\..\\..\\inputs\\4 -До 1000"), "*.html");
+var htmlFiles = Directory.GetFiles(Path.GetFullPath("..\\..\\..\\..\\inputs\\4 до и выше"), "*.html");
 processor.ProcessFiles(htmlFiles);
